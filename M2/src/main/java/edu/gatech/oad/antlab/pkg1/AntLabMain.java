0fa9a52e75607034f1238e402c1d28b8e5bbad95
saimghaya <saimghaya@gmail.com>
2019-01-25 13:37:56 -0500

    Updated AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("hhamid6");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
+                  Person5 p5 = new Person5("saim");
                   toPrint += p5.toString("sghaya3");
 
         System.out.println(toPrint);
